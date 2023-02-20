a5a82a69cca9fde11514f6a157ec1ca924430572
jusko1505 <justinkousa@gmaillcom>
2023-02-19 21:16:52 -0800

    commit

--- http_api.py
+++ http_api.py
@@ -3,13 +3,9 @@ import json
 
 app = Flask(__name__)
 
-
-
 @app.route("/simple_string")
 def return_a_simple_string():
     return "this is a simple string returned"
-
-
 
 app.run(host = "0.0.0.0")
     
