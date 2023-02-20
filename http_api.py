15b9d8909288f1121ec6abe71d1527a0d24c0307
jusko1505 <97807951+jusko1505@users.noreply.github.com>
2023-02-19 21:14:54 -0800

    Add files via upload

---
+++ http_api.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+
+
+@app.route("/simple_string")
+def return_a_simple_string():
+    return "this is a simple string returned"
+
+
+
+app.run(host = "0.0.0.0")
+    
